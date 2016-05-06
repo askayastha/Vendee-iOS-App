312ea9fb1e216e7d76a8bb7fb7a3daf8a693bde3
Ashish Kayastha <ashishk.48@gmail.com>
2016-05-06 21:35:07 +0545

    Changed the NSUserActivityType for web page URL handoff.

--- FlickShop/ContainerWebViewController.swift
+++ FlickShop/ContainerWebViewController.swift
@@ -51,7 +51,7 @@ class ContainerWebViewController: UIViewController {
         super.viewDidAppear(animated)
         
         // Start NSUserActivity
-        let activity = NSUserActivity(activityType: "com.ashish.vendee.webview")
+        let activity = NSUserActivity(activityType: "com.askayastha.vendee.webview")
         activity.title = "View Shopping URL"
         activity.webpageURL = webpageURL
         userActivity = activity
